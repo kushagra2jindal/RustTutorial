37c6ddd3129220950a71ff11fd36a741ea55d1de
kushagra2jindal <kushagra@integraate.com>
2023-04-15 19:49:23 +0530

    Ethereum Web3 get balance test!!

---
+++ hello_ethereum/src/main.rs
@@ -0,0 +1,25 @@
+#[tokio::main]
+async fn main() -> web3::Result<()> {
+
+    let transport = web3::transports::Http::new("https://mainnet.infura.io/v3/1130d8820d9b4b969d222c78c0a71b29")?;
+    let web3 = web3::Web3::new(transport);
+
+    println!("Calling accounts.");
+    let mut accounts = web3.eth().accounts().await?;
+    println!("Accounts: {:?}", accounts);
+    accounts.push("0x6Fb447Ae94F5180254D436A693907a1f57696900".parse().unwrap());
+
+    println!("Calling balance.");
+    for account in accounts {
+        let balance = web3.eth().balance(account, None).await?;
+        println!("Balance of {:?}: {}", account, balance);
+    }
+
+    Ok(())
+}
+
+fn get_balance(web3 : web3::Web3) {
+
+    println!("1");
+
+}
