f7e2189da7aa4961c42783b9de2add3bd415ab43
kushagra2jindal <kushagra@integraate.com>
2023-04-16 16:11:31 +0530

    Testing around with ethereum web3

--- hello_ethereum/src/main.rs
+++ hello_ethereum/src/main.rs
@@ -4,22 +4,29 @@ async fn main() -> web3::Result<()> {
     let transport = web3::transports::Http::new("https://mainnet.infura.io/v3/1130d8820d9b4b969d222c78c0a71b29")?;
     let web3 = web3::Web3::new(transport);
 
-    println!("Calling accounts.");
-    let mut accounts = web3.eth().accounts().await?;
-    println!("Accounts: {:?}", accounts);
-    accounts.push("0x6Fb447Ae94F5180254D436A693907a1f57696900".parse().unwrap());
+    // println!("Calling accounts.");
+    // let mut accounts = web3.eth().accounts().await?;
+    // println!("Accounts: {:?}", accounts);
+    // accounts.push("0x6Fb447Ae94F5180254D436A693907a1f57696900".parse().unwrap());
 
-    println!("Calling balance.");
-    for account in accounts {
-        let balance = web3.eth().balance(account, None).await?;
-        println!("Balance of {:?}: {}", account, balance);
-    }
+    // println!("Calling balance.");
+    // for account in accounts {
+    //     let balance = web3.eth().balance(account, None).await?;
+    //     println!("Balance of {:?}: {}", account, balance);
+    // }
+
+    get_balance(web3);
 
     Ok(())
 }
 
-fn get_balance(web3 : web3::Web3) {
+async fn get_balance(web3 : web3::Web3<Transport>) {
 
-    println!("1");
+    let mut accounts = web3.eth().accounts().await?;
+    accounts.push("0x6Fb447Ae94F5180254D436A693907a1f57696900".parse().unwrap());
+    for account in accounts {
+        let balance = web3.eth().balance(account, None).await?;
+        println!("Balance of {:?}: {}", account, balance);
+    }
 
 }
